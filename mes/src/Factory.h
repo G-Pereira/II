7fef5707e1a6b58142d81d0903fa640b18e39e71
Rafael <up201504487@fe.up.pt>
2019-06-02 23:50:37 +0100

    VERSÃO FINAL (FALTA ERP)

--- mes/src/Factory.h
+++ mes/src/Factory.h
@@ -32,31 +32,31 @@ public:
 	// Matrix the defines what is the top most machine that a transformation is going to use
 	// First index is the initial type and second index is final type (both with an offset of one due to vectors starting at zero)
 	// Used to determine availabilities of cells
-	vector<vector<char>> topMachine = {		// TODO: availability should change differently for AA transformation
-		{'0', 'A', 'A', 'A', 'A', '0', 'A', 'A', '0'},
+	vector<vector<char>> topMachine = {
+		{'0', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A'},
 		{'0', '0', 'B', '0', 'C', 'A', '0', '0', '0'},
 		{'0', '0', '0', '0', '0', '0', '0', '0', '0'},
 		{'0', '0', '0', '0', 'B', 'A', '0', '0', '0'},
 		{'0', '0', '0', '0', '0', 'A', '0', '0', '0'},
 		{'0', '0', '0', '0', '0', '0', '0', '0', '0'},
-		{'0', '0', '0', '0', '0', '0', '0', 'C', 'C'},
-		{'0', '0', '0', '0', 'A', '0', '0', '0', 'C'},
+		{'0', '0', '0', '0', 'A', 'A', '0', 'C', 'C'},
+		{'0', '0', '0', '0', 'A', 'A', '0', '0', 'C'},
 		{'0', '0', '0', '0', 'A', 'A', '0', '0', '0'}
 	};
 
 	// Matrix that defines on which type of cells certain transformations can be performed
 	// First index is the initial type and second index is final type (both with an offset of one due to vectors starting at zero)
 	// 0 means it can be performed on any cell; 1 means it can only be performed on cells 1 and 3; 2 means it can only be perfomed on cells 2 and 4
-	vector<vector<uint8_t>> possibleCells = {		// TODO
-		{0, 0, 1, 0, 1, 0, 0, 2, 0},
+	vector<vector<uint8_t>> possibleCells = {
+		{0, 0, 1, 0, 0, 2, 0, 2, 2},
 		{0, 0, 1, 0, 2, 2, 0, 0, 0},
 		{0, 0, 0, 0, 0, 0, 0, 0, 0},
 		{0, 0, 0, 0, 1, 1, 0, 0, 0},
 		{0, 0, 0, 0, 0, 0, 0, 0, 0},
 		{0, 0, 0, 0, 0, 0, 0, 0, 0},
-		{0, 0, 0, 0, 0, 0, 2, 2, 0},
-		{0, 0, 0, 0, 2, 0, 0, 0, 2},
-		{0, 0, 0, 0, 0, 1, 0, 0, 0}
+		{0, 0, 0, 0, 2, 2, 0, 2, 2},
+		{0, 0, 0, 0, 2, 2, 0, 0, 2},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0}
 	};
 
 
